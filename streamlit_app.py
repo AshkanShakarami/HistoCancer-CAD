cbc5d57e92a387612dd1d49c5fe3dd66173705aa
Ashkan Shakarami <101816571+AshkanShakarami@users.noreply.github.com>
2024-01-23 11:21:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image", use_column_width=True)
     st.write("Classifying...")  # You can add your image processing logic here'''
 
-import streamlit as st
+'''import streamlit as st
 import os
 
 # Title
@@ -37,7 +37,35 @@ if uploaded_file is not None:
     # You can add more interactive features or analysis logic here
 
     # Remove the temporary file
+    os.remove("temp.svs")'''
+
+import streamlit as st
+from PIL import Image
+import io
+
+# Title
+st.title("Whole Slide Image Viewer")
+
+# File uploader widget
+uploaded_file = st.file_uploader("Choose a .svs file...", type="svs")
+
+# Check if an image is uploaded
+if uploaded_file is not None:
+    # Save the uploaded file locally
+    with open("temp.svs", "wb") as f:
+        f.write(uploaded_file.getvalue())
+
+    # Open the slide image using PIL
+    slide_image = Image.open("temp.svs")
+
+    # Display the image using Streamlit
+    st.image(slide_image, caption="Whole Slide Image", use_column_width=True)
+
+    # You can add more interactive features or analysis logic here
+
+    # Remove the temporary file
     os.remove("temp.svs")
+
 
 
 

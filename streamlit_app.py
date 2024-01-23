21c9f1c513fc6dbe60ec1ce67c9159db1806abbb
Ashkan Shakarami <101816571+AshkanShakarami@users.noreply.github.com>
2024-01-23 10:36:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image", use_column_width=True)
     st.write("Classifying...")  # You can add your image processing logic here'''
 
-'''import streamlit as st
+import streamlit as st
 import os
 
 # Title
@@ -37,31 +37,9 @@ if uploaded_file is not None:
     # You can add more interactive features or analysis logic here
 
     # Remove the temporary file
-    os.remove("temp.svs")'''
+    os.remove("temp.svs")
 
-import streamlit as st
-import pyvips
-from io import BytesIO
 
-# Title
-st.title("Whole Slide Image Viewer")
-
-# File uploader widget
-uploaded_file = st.file_uploader("Choose a .svs file...", type="svs")
-
-# Check if an image is uploaded
-if uploaded_file is not None:
-    # Read the image using PyVips
-    slide = pyvips.Image.new_from_buffer(uploaded_file.getvalue(), "")
-
-    # Convert to RGB
-    rgb_image = slide.colourspace("srgb")
-
-    # Convert to PIL Image for display
-    pil_image = Image.fromarray(rgb_image.to_memoryview().tobytes(), 'RGB')
-
-    # Display the image
-    st.image(pil_image, caption="Slide Image", use_column_width=True)
 
 
 

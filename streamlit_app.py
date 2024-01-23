0ad2aa71439512333a57ca11b2e5f2845e75f02c
Ashkan Shakarami <101816571+AshkanShakarami@users.noreply.github.com>
2024-01-23 17:53:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image", use_column_width=True)
     st.write("Classifying...")  # You can add your image processing logic here'''
 
-import streamlit as st
+'''import streamlit as st
 from PIL import Image
 import io
 import os
@@ -42,7 +42,41 @@ if uploaded_file is not None:
     # Add more interactive features or analysis logic here
 
     # Remove the temporary file after the image is displayed
+    os.remove("temp.svs")'''
+
+import streamlit as st
+from PIL import Image
+import io
+import os
+import openslide
+import subprocess
+
+# Title
+st.title("HistoCancer-CAD: Whole Slide Image Viewer")
+
+# File uploader widget
+uploaded_file = st.file_uploader("Choose a .svs file...", type="svs")
+
+# Check if an image is uploaded
+if uploaded_file is not None:
+    # Save the uploaded file locally
+    with open("temp.svs", "wb") as f:
+        f.write(uploaded_file.getvalue())
+
+    # Open the slide image using OpenSlide
+    slide = openslide.OpenSlide("temp.svs")
+
+    # Generate a Deep Zoom Image (DZI) pyramid
+    dzi_file = "temp.dzi"
+    subprocess.run(["openslide-deepzoom", "deepzoom", "convert", "temp.svs", dzi_file])
+
+    # Display the DZI pyramid using OpenSeadragon
+    st.write('<iframe src="viewer.html" width="100%" height="800px"></iframe>', unsafe_allow_html=True)
+
+    # Remove the temporary files
     os.remove("temp.svs")
+    os.remove(dzi_file)
+
 
 
 

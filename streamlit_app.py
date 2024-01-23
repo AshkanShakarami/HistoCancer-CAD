613989381b05232a44ba9923a12beaeda57934de
Ashkan Shakarami <101816571+AshkanShakarami@users.noreply.github.com>
2024-01-23 11:27:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,28 +16,7 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image", use_column_width=True)
     st.write("Classifying...")  # You can add your image processing logic here'''
 
-'''import streamlit as st
-import os
 
-# Title
-st.title("Whole Slide Image Viewer")
-
-# File uploader widget
-uploaded_file = st.file_uploader("Choose a .svs file...", type="svs")
-
-# Check if an image is uploaded
-if uploaded_file is not None:
-    # Save the uploaded file locally
-    with open("temp.svs", "wb") as f:
-        f.write(uploaded_file.getvalue())
-
-    # Use the openslide executable to open and read the WSI
-    os.system(f"openslide-show-properties temp.svs")
-
-    # You can add more interactive features or analysis logic here
-
-    # Remove the temporary file
-    os.remove("temp.svs")'''
 import streamlit as st
 from PIL import Image
 import io

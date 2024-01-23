73b21e07dca668c72d993cb966dfbce64ec3936a
Ashkan Shakarami <101816571+AshkanShakarami@users.noreply.github.com>
2024-01-23 10:21:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,18 @@
 import streamlit as st
-st.markdown("Hey Gustavo")
+from PIL import Image
+
+# Title
+st.title("Streamlit Image Loader")
+
+# File uploader widget
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+# Check if an image is uploaded
+if uploaded_file is not None:
+    # Read the image
+    image = Image.open(uploaded_file)
+
+    # Display the image
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.write("Classifying...")  # You can add your image processing logic here
+
